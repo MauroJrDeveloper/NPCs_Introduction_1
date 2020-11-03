c0cf13cafbbd1b98ce509c1f903e0f72d8ef41f4
MauroJrDeveloper <59150284+MauroJrDeveloper@users.noreply.github.com>
2020-11-03 16:41:38 -0300

    Project

---
+++ Assets/Scripts/Dialog_System/Reset_Dialogs.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Reset_Dialogs : MonoBehaviour
+{
+
+    public Dialog_Controller _Dialog_Controller_Script;
+    public Dialog_Box _Dialog_Box_Script;
+
+    // Called in User_Interaction class, after the Key F to be pressed.
+
+    public void Reset_Dialog()
+    {
+        _Dialog_Controller_Script._User_Speech_Counter = 0;
+        _Dialog_Controller_Script._NPC_Speech_Counter = 0;
+        _Dialog_Controller_Script._Dialog_Text.text = "";
+        _Dialog_Controller_Script._Dialog_Name.text = "";
+        _Dialog_Box_Script.Close_Dialog_Box();
+    }
+
+}
