7c505db082b180e723bea05819198eda68cf39ab
MauroJrDeveloper <59150284+MauroJrDeveloper@users.noreply.github.com>
2020-11-18 09:13:42 -0300

    Update Reset_Dialogs.cs

--- Assets/Scripts/Dialog_System/Reset_Dialogs.cs
+++ Assets/Scripts/Dialog_System/Reset_Dialogs.cs
@@ -5,6 +5,7 @@ public class Reset_Dialogs : MonoBehaviour
 
     public Dialog_Controller _Dialog_Controller_Script;
     public Dialog_Box _Dialog_Box_Script;
+    public GameObject _Dialog_Box;
 
     // Called in User_Interaction class, after the Key F to be pressed.
 
@@ -14,7 +15,7 @@ public class Reset_Dialogs : MonoBehaviour
         _Dialog_Controller_Script._NPC_Speech_Counter = 0;
         _Dialog_Controller_Script._Dialog_Text.text = "";
         _Dialog_Controller_Script._Dialog_Name.text = "";
-        _Dialog_Box_Script.Close_Dialog_Box();
+        _Dialog_Box.SetActive(false);
     }
 
 }
